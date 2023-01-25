14f72519503b3823456cb4b135fe63d982361c82
Prins Kamariya <kamriyaprince@gmail.com>
2023-01-25 14:17:29 +0530

    http module

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Check about OS , path module etc and third party modules
+const http = require("http");
+const fs = require("fs");
+
+const PORT = process.env.PORT || 3000;
+const home = fs.readFileSync("./index.html", "utf-8");
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    return res.end(home);
+  }
+  if (req.url == "/about") {
+    return res.end("<h1>About Page</h1>");
+  }
+  if (req.url == "/contact") {
+    return res.end("<h1>Contact Page</h1>");
+  }
+});
+
+server.listen(PORT,()=>{
+    console.log(`Server started on PORT NUMBER: ${PORT}`);
+})
